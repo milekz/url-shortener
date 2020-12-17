6d0cdd8aea83cc90c75009b0a535b4dc3d3dd3a9
milek <milekz@gmail.com>
2020-12-17 18:46:36 +0100

    c1

--- src/models/database.js
+++ src/models/database.js
@@ -7,8 +7,8 @@ var settings = require('../settings/settings')
 
 // Create schemas and models for mongo
 var urlSchema = mongoose.Schema({
-    url: String,
-    short: String
+  url: String,
+  short: String
 });
 var urlModel = mongoose.model('url', urlSchema)
 
@@ -16,7 +16,7 @@ var urlModel = mongoose.model('url', urlSchema)
 module.exports = {
   db: null,
 
-  connect: function(){
+  connect: function () {
     mongoose.connect('mongodb://' + settings.dbhost + ":" + settings.dbport + '/' + settings.dbname)
     this.db = mongoose.connection
     this.db.on('error', console.error.bind(console, 'connection error:'))
@@ -25,54 +25,61 @@ module.exports = {
     });
   },
 
-  get: function(short, callback){
+  get: function (short, callback) {
     console.log("Finding " + short)
-    urlModel.findOne({ 'short': short}, {}, { sort: {"created_at": -1} }, function (err, url) {
+    urlModel.findOne({ 'short': short }, {}, { sort: { "created_at": -1 } }, function (err, url) {
       console.log(url)
       callback(url)
     })
 
   },
 
-  check_short: function(short, callback){
-    urlModel.findOne({ 'short': short}, function (err, url) {
+  get_all: function (short, callback) {
+    urlModel.find({}, function (err, url) {
+      //console.log(url)
+      callback(url)
+    })
+  },
+
+  check_short: function (short, callback) {
+    urlModel.findOne({ 'short': short }, function (err, url) {
       callback(null, url)
     })
   },
 
-  check_url: function(url, callback){
-    urlModel.findOne({ 'url': url}, function (err, url) {
+  check_url: function (url, callback) {
+    urlModel.findOne({ 'url': url }, function (err, url) {
       callback(err, url)
     })
   },
 
-  generate_short: function(){
+  generate_short: function () {
     short = randomstring.generate(settings.shortlength)
     return short
   },
 
-  create: function(url, short, callback){
+  create: function (url, short, callback) {
     var db = this
 
-    if (short === undefined){
+    if (short === undefined) {
       short = db.generate_short()
       userdefshort = false
     } else {
       userdefshort = true
     }
 
-    db.check_short(short, function(err, result){
-      if (result){
+    db.check_short(short, function (err, result) {
+      if (result) {
         console.log("Short code already exists")
         callback("Short code already exists", null)
       } else {
-        db.check_url(url, function(err, result){
-          if (result && ! userdefshort){
+        db.check_url(url, function (err, result) {
+          if (result && !userdefshort) {
             callback(null, result)
           } else {
-            var newUrl = new urlModel({"url" : url, "short": short})
-            newUrl.save(function(error){
-              if (error){
+            var newUrl = new urlModel({ "url": url, "short": short })
+            newUrl.save(function (error) {
+              if (error) {
                 console.log("Write to mongo failed")
                 console.log(error)
                 callback("Write to mongo failed", null)
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@
 var express = require('express')
 var router = express.Router()
 
+
 // Load project scripts
 var db = require('../models/database')
 var settings = require('../settings/settings')
@@ -10,15 +11,15 @@ var settings = require('../settings/settings')
 db.connect()
 
 // Utility functions
-var build_response = function(status, message, result){
+var build_response = function (status, message, result) {
   return {
-    "status" : status,
-    "message" : message,
-    "result" : result
+    "status": status,
+    "message": message,
+    "result": result
   }
 }
 
-var respond = function(res, data){
+var respond = function (res, data) {
   console.log(data)
   res.setHeader('Content-Type', 'application/json')
   res.status(data.status)
@@ -32,9 +33,22 @@ router.get('/', function (req, res) {
 })
 
 router.get('/check/:short', function (req, res) {
-  db.check_short(req.params.short, function(err, url){
-    if (url){
-      var data = build_response(200, "Short exists", {"url": url.url, "short": url.short, "baseurl": settings.getBaseURL()})
+  db.check_short(req.params.short, function (err, url) {
+    if (url) {
+      var data = build_response(200, "Short exists", { "url": url.url, "short": url.short, "baseurl": settings.getBaseURL() })
+    } else {
+      var data = build_response(404, "Short not found", null)
+    }
+    respond(res, data)
+  })
+})
+
+router.get('/getall', function (req, res) {
+  db.get_all(null, function (url) {
+    //console.log(url)
+    if (url) {
+      var answer = '';
+      var data = build_response(200, "Short exists", url)
     } else {
       var data = build_response(404, "Short not found", null)
     }
@@ -44,17 +58,17 @@ router.get('/check/:short', function (req, res) {
 
 router.post('/create', function (req, res) {
   console.log(req.body.url)
-  if (req.body.url === undefined){
+  if (req.body.url === undefined) {
     console.log("Missing url")
     var data = build_response(400, "Missing url", null)
     respond(res, data)
   } else {
     console.log("Creating url")
-    db.create(req.body.url, req.body.short, function(err, creation){
+    db.create(req.body.url, req.body.short, function (err, creation) {
       console.log("DB request made")
       if (creation) {
         console.log("Success, short created!")
-        var data = build_response(201, "Success, short created!", {"url" : creation.url, "short" : creation.short, "baseurl": settings.getBaseURL()})
+        var data = build_response(201, "Success, short created!", { "url": creation.url, "short": creation.short, "baseurl": settings.getBaseURL() })
       } else {
         console.log("Failed to create")
         var data = build_response(400, "Failed to create: " + err, null)
@@ -67,7 +81,7 @@ router.post('/create', function (req, res) {
 
 router.get('/genshort', function (req, res) {
   var short = db.generate_short()
-  var data = build_response(200, "Generated", {"short": short, "baseurl": settings.getBaseURL()})
+  var data = build_response(200, "Generated", { "short": short, "baseurl": settings.getBaseURL() })
   respond(res, data)
 })
 
